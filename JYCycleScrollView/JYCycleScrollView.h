5d7cb90c0f94a5a9480944f919515fe6b23a60b4
luoshimei0825 <452402a033v.me@gmail.com>
2023-03-27 11:47:45 +0800

    Initial Commit

---
+++ JYCycleScrollView/JYCycleScrollView.h
@@ -0,0 +1,18 @@
+//
+//  JYCycleScrollView.h
+//  JYCycleScrollView
+//
+//  Created by luoshimei on 2023/3/27.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JYCycleScrollView.
+FOUNDATION_EXPORT double JYCycleScrollViewVersionNumber;
+
+//! Project version string for JYCycleScrollView.
+FOUNDATION_EXPORT const unsigned char JYCycleScrollViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JYCycleScrollView/PublicHeader.h>
+
+
